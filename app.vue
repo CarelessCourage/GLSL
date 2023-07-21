22bcbb156faf528948ad3271930e8291295b140b
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-07-21 21:01:39 +0200

    adds ui

--- app.vue
+++ app.vue
@@ -1,19 +1,48 @@
 // App.vue
 <script setup lang="ts">
+import { myriad } from "@myriadjs/core"
+import "./style.css"
+
+onMounted(() => {
+  const m = myriad({
+  background: '#0c0915',
+  foreground: '#c0aea3',
+  accents: ['#ab57ff'],
+}).attach().colors
+
+console.log(m)
+})
+
+const people = ['Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']
+const selected = ref(people[0])
 </script>
 
 <template>
-  <div class="tres">
+  <div  class="tres">
     <Spiral />
 
     <div class="meta-island">
       <div class="github">github</div>
-      
+      <USelectMenu v-model="selected" :options="people" />
     </div>
   </div>
 </template>
 
 
 <style>
-.tres {}
+.meta-island {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  background-color: var(--foreground);
+
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  justify-content: flex-start;
+  z-index: 999;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import glsl from 'vite-plugin-glsl';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@tresjs/nuxt"],
+  modules: ["@tresjs/nuxt", "@nuxthq/ui"],
   vite: {
     plugins: [
       glsl()
