aad2a2319b83c531db977237acd2d5755c8b5f78
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-08-22 14:32:42 +0200

    safeguard

--- composables/useFBO.ts
+++ composables/useFBO.ts
@@ -7,8 +7,8 @@ export const useFBO = (width?: number, height?: number, dpr = 500) => {
   
   const size = computed(() => {
     const sizes = state.sizes
-    const _width = width ? width * dpr : sizes.width.value
-    const _height = height ? height * dpr : sizes.height.value
+    const _width = width ? width * dpr : sizes?.width.value || 10 * dpr
+    const _height = height ? height * dpr : sizes?.height.value || 10 * dpr
     return {
       width: _width,
       height: _height,
