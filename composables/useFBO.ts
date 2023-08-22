6c362d9d4da7849feb5e8688cd831ec5d705a7b5
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-08-22 14:40:16 +0200

    adds bug checker

--- composables/useFBO.ts
+++ composables/useFBO.ts
@@ -6,6 +6,7 @@ export const useFBO = (width?: number, height?: number, dpr = 500) => {
   const renderTarget = shallowRef<THREE.WebGLRenderTarget>(new THREE.WebGLRenderTarget(10 * dpr, 10 * dpr))
   
   const size = computed(() => {
+    console.log('size', state)
     const sizes = state.sizes
     const _width = width ? width * dpr : sizes?.width.value || 10 * dpr
     const _height = height ? height * dpr : sizes?.height.value || 10 * dpr
